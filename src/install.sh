1fe469b375e5b0100160517b7771db2101a70838
joeunlog <j.eunvit@gmail.com>
2023-10-12 13:19:26 +0900

    remove go-cron, aws secret env

--- src/install.sh
+++ src/install.sh
@@ -16,13 +16,13 @@ apk add py3-pip  # separate package on edge only
 pip3 install awscli
 
 # install go-cron
-apk add curl
-curl -L https://github.com/ivoronin/go-cron/releases/download/v0.0.5/go-cron_0.0.5_linux_${TARGETARCH}.tar.gz -O
-tar xvf go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
-rm go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
-mv go-cron /usr/local/bin/go-cron
-chmod u+x /usr/local/bin/go-cron
-apk del curl
+# apk add curl
+# curl -L https://github.com/ivoronin/go-cron/releases/download/v0.0.5/go-cron_0.0.5_linux_${TARGETARCH}.tar.gz -O
+# tar xvf go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
+# rm go-cron_0.0.5_linux_${TARGETARCH}.tar.gz
+# mv go-cron /usr/local/bin/go-cron
+# chmod u+x /usr/local/bin/go-cron
+# apk del curl
 
 
 # cleanup
--- src/run.sh
+++ src/run.sh
@@ -6,8 +6,16 @@ if [ "$S3_S3V4" = "yes" ]; then
   aws configure set default.s3.signature_version s3v4
 fi
 
-if [ -z "$SCHEDULE" ]; then
+if [ "$BACKUP" = yes ]; then
   sh backup.sh
 else
-  exec go-cron "$SCHEDULE" /bin/sh backup.sh
+  sh restore.sh
 fi
+
+
+
+# if [ -z "$SCHEDULE" ]; then
+#   sh backup.sh
+# else
+#   exec go-cron "$SCHEDULE" /bin/sh backup.sh
+# fi
